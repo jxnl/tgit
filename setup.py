a3a833d7df2b0d0467e56ab0ccc9e7bec7de3b8e
Jason Liu <jliu@marketwired.com>
2014-09-11 12:06:15 -0400

    inital commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="tgit",
+    version='0.1.0',
+    author='Jason Liu',
+    author_email='jx5liu@uwaterloo.ca',
+    url="http://jxnl.co",
+    packages=['tgit'],
+    scripts=['tgit/tgit'],
+    license='LICENSE.txt',
+    description="""Tweet Those Commits""",
+    long_description=open("README.md").read(),
+    install_requires=['envoy', 'twitter', 'argparse'],
+)
