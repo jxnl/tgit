843ebe9ad20941dab27457bffb9c5796235bad62
Jason Liu <jx5liu@uwaterloo.ca>
2014-09-19 00:27:34 -0400

    updated to 0.1.1 supports twitter searching

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tgit",
-    version='0.1.0',
+    version='0.1.1',
     author='Jason Liu',
     author_email='jx5liu@uwaterloo.ca',
     url="http://jxnl.co",
